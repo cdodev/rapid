62ae2da4e1366cb2b4f26e55ed29801710801f8b
Ben Ford <ben@commando.dev>
2020-12-30 15:54:08 +0000

    Remove broken symlink

--- nt.nix
+++
@@ -1,8 +0,0 @@
-let
-  haskellNix = import (builtins.fetchTarball https://github.com/input-output-hk/haskell.nix/archive/master.tar.gz) {};
-  nixpkgs = import haskellNix.sources.nixpkgs-1909 haskellNix.nixpkgsArgs;
-  haskell = nixpkgs.haskell-nix;
-in
-  haskell
-  # haskell.haskellPackages.ghcWithPackages (ps: with ps;
-  #   [ lens conduit conduit-extra ])
